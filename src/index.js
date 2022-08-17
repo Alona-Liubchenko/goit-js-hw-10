612796b24d7bca5a8a590d376c58734918826371
Alona-Liubchenko <aylin888@rambler.ru>
2022-08-17 23:15:50 +0300

    h3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
+const inputEl = document.querySelector('#search-box');
+const ulEl = document.querySelector('.country-list');
+const divEl = document.querySelector('.country-info');
+
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v2/name/{name}')
+  .then(response => {
+    return response.json();
+  })
+  .then(fetchCountries => console.log(fetchCountries))
+  .catch(error => {
+    console.log(error);
+  });
