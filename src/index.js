4a4347a678efc70acfe4c8f2c62035dbaffa2854
Alona-Liubchenko <aylin888@rambler.ru>
2022-08-19 01:53:36 +0300

    h4

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const divEl = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
 
-fetch('https://restcountries.com/v2/name/{name}')
-  .then(response => {
-    return response.json();
-  })
-  .then(fetchCountries => console.log(fetchCountries))
-  .catch(error => {
-    console.log(error);
-  });
+const onInputSearch = e => {
+  e.preventDefault();
+  const form = e.currentTarget;
+
+  let queryEl = form.elements.query.value;
+  console.log(queryEl);
+};
+inputEl.addEventListener('input', onInputSearch);
