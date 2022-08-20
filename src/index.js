5cc2d89ff9dfdf05527f5017399e86e361145a3d
Alona-Liubchenko <aylin888@rambler.ru>
2022-08-21 01:41:48 +0300

    h6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
-import API from './fetchCountries';
+// import { fetchCountries } from './fetchCountries';
+
 const inputEl = document.querySelector('#search-box');
 const ulEl = document.querySelector('.country-list');
 const divEl = document.querySelector('.country-info');
@@ -10,26 +11,15 @@ const DEBOUNCE_DELAY = 300;
 
 function onSearch(e) {
   e.preventDefault();
-  const input = e.currentTarget;
-  console.log(input.elements);
-  const searchInput = inputEl.value.trim();
-
-  API.fetchCountries(searchInput)
-    .then(renderCauntrysCard)
-    .catch(error => console.log(error));
-}
-function cauntryList() {
-  return `<div>
-<h1>${fetchCountries(name)}</h1>
-</div>`;
+  const name = e.target.value.trim();
+  console.log(name);
+  fetchCountries(name);
 }
 
-function renderCauntrysCard(cauntry) {
-  const markup = cauntryList(cauntry);
-  console.log(markup);
-  divEl.innerHTML = markup;
-}
-
-function onFetchError(error) {
-  alert('Oops, there is no country with that name');
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(response => {
+      return response.json();
+    })
+    .then();
 }
