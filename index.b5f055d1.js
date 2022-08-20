f059760fc8f169d483585a189b94e050938b0e35
Alona-Liubchenko <98355708+Alona-Liubchenko@users.noreply.github.com>
2022-08-20 00:03:56 +0000

    Deploying to gh-pages from @ Alona-Liubchenko/goit-js-hw-10@960ae42f952f2a94f5c5c7d05680655ba1f14e00 🚀

--- index.b5f055d1.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#search-box");document.querySelector(".country-list"),document.querySelector(".country-info");e.addEventListener("input",(function(e){e.preventDefault();var t=e.currentTarget.elements.query.value;console.log(t)}))}();
-//# sourceMappingURL=index.b5f055d1.js.map
