dd8b1c89cc5877c54c774fafd9353f12c4ac2b01
Alona-Liubchenko <98355708+Alona-Liubchenko@users.noreply.github.com>
2022-08-22 00:21:22 +0000

    Deploying to gh-pages from @ Alona-Liubchenko/goit-js-hw-10@78ecc50c3f35f32c209334faed10e491ca4a9113 🚀

--- index.de4ae761.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#search-box");document.querySelector(".country-list"),document.querySelector(".country-info");e.addEventListener("input",(function(e){e.preventDefault();const t=e.target.value.trim();console.log(t),function(e){fetch(`https://restcountries.com/v3.1/name/${e}`).then((e=>e.json())).then()}(t)}));
-//# sourceMappingURL=index.de4ae761.js.map
